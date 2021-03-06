8eb1ef59b1ea93bde1d76613b693a4f9fa28e04c
Dinis Medeiros <formadordinis@gmail.com>
2021-03-06 00:19:52 +0000

    Create pt.php
    Tradução em português de Portugal

---
+++ lang/pt.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_timeline', language 'pt', version '3.8'.
+ *
+ * @package     format_timeline
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['allowsubmissionscutoffdatesummary'] = 'Está atrasado mas pode submeter até <strong>{$a}</strong>';
+$string['allowsubmissionsfromdatesummary'] = 'Este trabalho aceita submissões a partir de <strong>{$a}</strong>';
+$string['allowsubmissionsuntildatesummary'] = 'Este trabalho aceita submissões até <strong>{$a}</strong>';
+$string['closed'] = 'Fechado';
+$string['comment'] = 'Comentário';
+$string['courseeditingison'] = 'O modo de edição está ativo';
+$string['courseeditingisonalert'] = 'Não se esqueça de desativar após concluir todas as alterações!';
+$string['coursepost'] = 'Publicação na disciplina';
+$string['createpost'] = 'Criar uma nova publicação';
+$string['createpost_validator_message'] = 'A publicação tem de ter pelo menos 10 caracteres.';
+$string['delayed'] = 'Atrasado';
+$string['deletepost'] = 'Apagar publicação';
+$string['deletepost_confirm_msg'] = 'Depois de apagada, a publicação não poderá ser recuperada!';
+$string['deletepost_confirm_no'] = 'Cancelar';
+$string['deletepost_confirm_title'] = 'Tem a certeza?';
+$string['deletepost_confirm_yes'] = 'Sim, quero apagar!';
+$string['deletepost_success'] = 'Publicação apagada com sucesso.';
+$string['hidefromothers'] = 'Ocultar linha do tempo';
+$string['loadmore'] = 'Mostrar mais {$a} comentários';
+$string['message_clicktoaccesspost'] = 'Clique aqui para aceder à publicação';
+$string['message_contextname'] = 'Publicações da Linha do tempo';
+$string['message_mentioncontextname'] = 'Menção numa publicação.';
+$string['message_mentionuserementioned'] = 'Foi mencionado numa publicação';
+$string['message_mentionuserementionedincourse'] = 'Foi mencionado numa publicação na disciplina <b>{$a}</b>';
+$string['message_newpostinacourse'] = 'Existe uma nova mensagem numa disciplina';
+$string['message_newpostinthecourse'] = 'Existe uma nova mensagem na sua disciplina';
+$string['messageprovider:postmention'] = 'Notificação de menção numa publicação';
+$string['messageprovider:timelineposts'] = 'Notificação de novas publicações do formato de disciplina Linha do Tempo';
+$string['notopenedyet'] = 'Fechado';
+$string['onlyactivities'] = 'Apenas atividades e recursos';
+$string['onlyenrolleduserscancomment'] = 'Apenas os utilizadores inscritos podem adicionar comentários.';
+$string['onlyenrolleduserscanviewposts'] = 'Apenas os utilizadores inscritos podem ver todos os comentários.';
+$string['onlyposts'] = 'Apenas publicações';
+$string['open'] = 'Submissão permitida';
+$string['orderasc'] = 'Mais novos primeiro';
+$string['orderdesc'] = 'Mais antigos primeiro';
+$string['pluginname'] = 'Linha do tempo';
+$string['postcreated'] = 'Publicação criada com sucesso.';
+$string['postdeleted'] = 'Publicação apagada com sucesso.';
+$string['postedon'] = 'Publicado:';
+$string['privacy:metadata'] = 'O módulo Linha do tempo social não armazena quaisquer dados pessoais.';
+$string['sectionname'] = 'Linha do tempo';
+$string['showall'] = 'Mostrar tudo';
+$string['showfromothers'] = 'Mostrar linha do tempo';
+$string['wanttodo'] = 'Professor, o que pretende fazer?';
+$string['writeacomment'] = 'Escreva um comentário';
+$string['writethepost'] = 'Digite o texto da publicação';
+$string['youcantaddpost'] = 'Não tem permissão para adicionar publicações nesta disciplina.';
+$string['youcantdeletepost'] = 'Não tem permissão para apagar esta publicação.';
+$string['youcantviewpost'] = 'Não tem permissão para visualizar esta publicação.';
