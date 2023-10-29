ae78fedfcd2ba47ccdcc72fb8c89bd393a877ffb
Willian Mano <willian.mano@jove.com>
2023-10-29 19:40:14 -0300

    Increase version nu

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023020500;
-$plugin->requires  = 2022041906;
+$plugin->version   = 2023100902;
+$plugin->requires  = 2023100400;
 $plugin->component = 'format_timeline';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.0.0';
+$plugin->release   = '4.3.0';
